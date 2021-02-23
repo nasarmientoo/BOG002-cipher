c3842868c5db80997ba644f92282992ce177bc0c
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-02-23 10:42:16 -0500

    First aproximation

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,13 @@
-const cipher = {
-  // ...
-};
+function cipher(desplazamiento,letrain){
+  var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  var letrain = letrain.toUpperCase();
+  var desplazamiento = parseInt(desplazamiento);
+  var posicionletra = abecedario.indexOf(letrain);
+  posicionletra=parseInt(posicionletra);
+  var letradesplazada = (posicionletra+desplazamiento)%26;
+  var resultado = abecedario[letradesplazada];
+  alert (resultado);
+
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function Ocultar (){
+    var section1 = document.getElementById("section1");
+    section1.style.display = "none"
+    var section2 = document.getElementById("section2");
+    section2.style.display = "none"
+    var section3 = document.getElementById("section3");
+    section3.style.display = "none"
+    var section4 = document.getElementById("section4");
+    section4.style.display = "none"
+    var section5 = document.getElementById("section5");
+    section5.style.display = "none"
+}
+ 
+function Continue_section2(){
+    Ocultar()
+    section2.style.display = "block"
+}
+ 
+function Cifrar_selection(){
+    Ocultar()
+    section3.style.display = "block"
+}
+ 
+function Descifrar_selection(){
+    Ocultar()
+    section4.style.display = "block"
+}
+ 
+function GuardarDatos(){
+    var des = document.getElementById("input_des").nodeValue;
+    var men = document.getElementById("input_men").nodeValue;
+    if (des === "" || men === ""){
+        alert("Faltan datos por ingresar")}
+    else if (typeof men === "number"){
+        alert("El mensaje solo puede contener letras")}
+    else if (typeof des === "string"){
+        alert("El desplazamiento es de tipo numerico")}
+    else {
+        Ocultar()
+        section5.style.display = "block" 
+        cipher(des, men)
+    }
+}
+
