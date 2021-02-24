47678b43143d36931af80877f2d1edd9c3415449
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-02-24 11:57:46 -0500

    last changes

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,7 @@
+const cipher = {
 // Cifrar
+encode: function(){
+  var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   document.getElementById ('valoresIniciales').addEventListener ('click',almacenarmesaje);
   function almacenarmesaje() {
   var mensaje = document.getElementById('input_men').value;
@@ -13,8 +16,26 @@
       let letradesplazada = (posicionletra+desplazamiento)%26;
       let resultado = abecedario[letradesplazada];
       alert (resultado);
-}
-    
-let abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  }
+},
+//Descifrar
+decode: function (){ 
+  var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  document.getElementById ('valoresIniciales').addEventListener ('click',almacenarmesaje);
+  function almacenarmesaje() {
+  var mensaje = document.getElementById('input_men').value;
+  mensaje = mensaje.toUpperCase ();
+  console.log (mensaje)
+  var desplazamiento =  document.getElementById('input_des').value
+  desplazamiento = parseInt(desplazamiento)
+  console.log (desplazamiento)
 
-export default cipher;
+  let posicionletra = abecedario.indexOf(mensaje);
+      posicionletra=parseInt(posicionletra);
+      let letradesplazada = (posicionletra-desplazamiento)%26;
+      let resultado = abecedario[letradesplazada];
+      alert (resultado);
+  }
+ }
+};
+
--- src/main.js
+++ src/main.js
@@ -7,8 +7,6 @@ function Ocultar (){
     section3.style.display = "none"
     var section4 = document.getElementById("section4");
     section4.style.display = "none"
-    var section5 = document.getElementById("section5");
-    section5.style.display = "none"
 }
  
 function Continue_section2(){
@@ -26,5 +24,20 @@ function Continue_section4(){
     section4.style.display = "block"   
 }
 
-export default ocultar;
+/*function GuardarDatos(){
+    var des = document.getElementById("input_des").nodeValue;
+    var men = document.getElementById("input_men").nodeValue;
+    if (des === "" || men === ""){
+        alert("Faltan datos por ingresar")}
+    else if (typeof men === "number"){
+        alert("El mensaje solo puede contener letras")}
+    else if (typeof des === "string"){
+        alert("El desplazamiento es de tipo numerico")}
+    else {
+        if 
+        Ocultar()
+        section5.style.display = "block" 
+        cipher.encode(des, men)
+    }    
+}*/
 
