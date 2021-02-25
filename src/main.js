49c06a9890fb42cc3eb5116fd37d05305cacc21a
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-02-25 13:44:18 -0500

    Last changes section 5 HTML

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import cipher from "./cipher.js";
 
-document.getElementById("Boton1").addEventListener("click",Continue_section2)
-document.getElementById("Boton2").addEventListener("click",Continue_section3)
-document.getElementById("Boton3").addEventListener("click",Continue_section4)
-document.getElementById("Boton5").addEventListener("click",Continue_section2)
-document.getElementById("Boton7").addEventListener("click",Continue_section2)
-document.getElementById("Boton8").addEventListener("click",Continue_principal)
+document.getElementById("Boton1").addEventListener("click",Continue_section2);
+document.getElementById("Boton2").addEventListener("click",Continue_section3);
+document.getElementById("Boton3").addEventListener("click",Continue_section4);
+document.getElementById("Boton5").addEventListener("click",Continue_section2);
+document.getElementById("Boton7").addEventListener("click",Continue_section2);
+document.getElementById("Boton8").addEventListener("click",Continue_principal);
 document.getElementById ("Boton4").addEventListener("click",cipher.encode);
+document.getElementById ("Boton4").addEventListener("click",Continue_section5);
 document.getElementById ("Boton6").addEventListener("click",cipher.decode);
+document.getElementById ("Boton7").addEventListener("click",Continue_section5);
 
 function Ocultar (){
     var section1 = document.getElementById("section1");
@@ -40,6 +42,11 @@ function Continue_section3(){
 function Continue_section4(){
     Ocultar()
     section4.style.display = "block"   
+}
+
+function Continue_section5(){
+    Ocultar()
+    section5.style.display = "block"   
 }
 
 
