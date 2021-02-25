66755e317fd64cfd6426a956f14508d81ffece44
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-02-25 11:58:19 -0500

    Funciona cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,41 +1,45 @@
+let abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
 const cipher = {
-// Cifrar
-encode: function(){
-  var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  document.getElementById ('valoresIniciales').addEventListener ('click',almacenarmesaje);
-  function almacenarmesaje() {
-  var mensaje = document.getElementById('input_men').value;
-  mensaje = mensaje.toUpperCase ();
-  console.log (mensaje)
-  var desplazamiento =  document.getElementById('input_des').value
-  desplazamiento = parseInt(desplazamiento)
-  console.log (desplazamiento)
+    // Cifrar
+    encode: function(){
+      var mensaje = document.getElementById('input_men').value;
+      mensaje = mensaje.toUpperCase ();
+      var arregloMensaje = mensaje.split ('')
+      var desplazamiento =  document.getElementById('input_des').value
+      desplazamiento = parseInt(desplazamiento)
+      let nuevaPosicionLetra = []
+        
+      for (var i = 0;i<arregloMensaje.length;i++) {
+          let posicionletra = abecedario.indexOf(arregloMensaje [i])
+          let indiceDesplazado = (posicionletra+desplazamiento)%26;
+          let letradesplazada = (abecedario[indiceDesplazado])
+          nuevaPosicionLetra.push (letradesplazada);   
+      }
 
-  let posicionletra = abecedario.indexOf(mensaje);
-      posicionletra=parseInt(posicionletra);
-      let letradesplazada = (posicionletra+desplazamiento)%26;
-      let resultado = abecedario[letradesplazada];
-      alert (resultado);
-  }
-},
-//Descifrar
-decode: function (){ 
-  var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  document.getElementById ('valoresIniciales').addEventListener ('click',almacenarmesaje);
-  function almacenarmesaje() {
-  var mensaje = document.getElementById('input_men').value;
-  mensaje = mensaje.toUpperCase ();
-  console.log (mensaje)
-  var desplazamiento =  document.getElementById('input_des').value
-  desplazamiento = parseInt(desplazamiento)
-  console.log (desplazamiento)
+      let mensajeCifrado = nuevaPosicionLetra.join ('');
+      console.log (mensajeCifrado)
+    },
 
-  let posicionletra = abecedario.indexOf(mensaje);
-      posicionletra=parseInt(posicionletra);
-      let letradesplazada = (posicionletra-desplazamiento)%26;
-      let resultado = abecedario[letradesplazada];
-      alert (resultado);
-  }
- }
+    //Descifrar
+    decode: function (){ 
+      var mensaje = document.getElementById('input_men2').value;
+      mensaje = mensaje.toUpperCase ();
+      var arregloMensaje = mensaje.split ('')
+      var desplazamiento =  document.getElementById('input_des2').value
+      desplazamiento = parseInt(desplazamiento)
+      let nuevaPosicionLetra = []
+      
+      for (var i = 0;i<arregloMensaje.length;i++) {
+          let posicionletra = abecedario.indexOf(arregloMensaje [i])
+          let indiceDesplazado = (posicionletra+desplazamiento)%26;
+          let letradesplazada = (abecedario[indiceDesplazado])
+          nuevaPosicionLetra.push (letradesplazada);         
+      }
+
+      let mensajeDesCifrado = nuevaPosicionLetra.join ('');
+      console.log (mensajeDesCifrado)
+    }
 };
 
+export default cipher;
--- src/main.js
+++ src/main.js
@@ -1,43 +1,48 @@
+import cipher from './cipher.js';
+
+document.getElementById('Boton1').addEventListener('click',Continue_section2)
+document.getElementById('Boton2').addEventListener('click',Continue_section3)
+document.getElementById('Boton3').addEventListener('click',Continue_section4)
+document.getElementById('Boton5').addEventListener('click',Continue_section2)
+document.getElementById('Boton7').addEventListener('click',Continue_section2)
+document.getElementById('Boton8').addEventListener('click',Continue_principal)
+document.getElementById ('Boton4').addEventListener('click',cipher.encode);
+document.getElementById ('Boton6').addEventListener('click',cipher.decode);
+
 function Ocultar (){
-    var section1 = document.getElementById("section1");
-    section1.style.display = "none"
-    var section2 = document.getElementById("section2");
-    section2.style.display = "none"
-    var section3 = document.getElementById("section3");
-    section3.style.display = "none"
-    var section4 = document.getElementById("section4");
-    section4.style.display = "none"
+    var section1 = document.getElementById('section1');
+    section1.style.display = 'none'
+    var section2 = document.getElementById('section2');
+    section2.style.display = 'none'
+    var section3 = document.getElementById('section3');
+    section3.style.display = 'none'
+    var section4 = document.getElementById('section4');
+    section4.style.display = 'none'
+    var section5 = document.getElementById('section5');
+    section5.style.display = 'none'
 }
- 
+
+function Continue_principal(){
+    Ocultar()
+    section1.style.display = 'block'
+}
+
 function Continue_section2(){
     Ocultar()
-    section2.style.display = "block"
+    section2.style.display = 'block'
 }
 
 function Continue_section3(){
     Ocultar()
-    section3.style.display = "block"
+    section3.style.display = 'block'
 }
 
 function Continue_section4(){
     Ocultar()
-    section4.style.display = "block"   
+    section4.style.display = 'block'   
 }
 
-/*function GuardarDatos(){
-    var des = document.getElementById("input_des").nodeValue;
-    var men = document.getElementById("input_men").nodeValue;
-    if (des === "" || men === ""){
-        alert("Faltan datos por ingresar")}
-    else if (typeof men === "number"){
-        alert("El mensaje solo puede contener letras")}
-    else if (typeof des === "string"){
-        alert("El desplazamiento es de tipo numerico")}
-    else {
-        if 
-        Ocultar()
-        section5.style.display = "block" 
-        cipher.encode(des, men)
-    }    
-}*/
+
+
+
 
